098a3020bb4bcfc360948088e836ebf53dbc6a48
R4FI3L <taverasrafiel@gmail.com>
2020-06-30 10:16:56 -0400

    Cambiando nombres de archivos

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 const CACHE_NAME = 'download_time_calc_cache',
   urlsToCache = [
     './',
-    'Index.html',
-    'Css/styles.css',
-    'Js/calc.js',
-    'Img/cronometer.png'
+    'index.html',
+    'css/styles.css',
+    'js/calc.js',
+    'img/cronometer.png'
 ]
