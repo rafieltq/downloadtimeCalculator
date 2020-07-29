684a0f049b2ebe91108eca40fc998fa502d6c130
R4FI3L <taverasrafiel@gmail.com>
2020-07-29 15:37:47 -0400

    All is configured

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,23 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+
 module.exports={
     entry: './src/js/calc.js',
     output:{
-        path: __dirname + '/build',
+        path: path.resolve(__dirname, './build'),
         filename:'bundle.js'
-    }
-}
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+        template: './src/index.html'
+    })
+],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+};
