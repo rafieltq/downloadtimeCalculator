80f55b3e679d2f60956666fb6281e2ca5da94a70
R4FI3L <taverasrafiel@gmail.com>
2020-07-31 18:59:26 -0400

    language configs

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -1,58 +1,80 @@
 //languages config
 const englishConfig = {
-    Peso: "File weight",
-    Velocidad: "Internet speed",
-    Calcular: "Calculate",
-    Resultado: "Here you will see how long your download will take.",
-    Verificar: "Don't you know what your internet speed is? You can check it at:",
-    Idiomas: "Languages"
+    weight: "File weight",
+    speed: "Internet speed",
+    calculate: "Calculate",
+    results: "Here you will see how long your download will take.",
+    doYouKnow: "Don't you know what your internet speed is? You can check it at:",
+    language: "Languages",
+    close:"Close",
+    downloadTime: "Your download will take",
+    hours:"hours",
+    minutes:"minutes",
+    seconds:"seconds",
+    day:'days'
 }
 
 const spanishConfig = {
-    Peso: "Peso del archivo",
-    Velocidad: "Velocidad de internet",
-    Calcular: "Calcular",
-    Resultado: "Aquí verá cuánto tardará su descarga.",
-    Verificar: "¿No sabes cuál es tu velocidad de internet? Puedes consultarlo en:",
-    Idiomas: "Idiomas"
+    weight: "Peso del archivo",
+    speed: "Velocidad de internet",
+    calculate: "Calcular",
+    results: "Aquí verá cuánto tardará su descarga.",
+    doYouKnow: "¿No sabes cuál es tu velocidad de internet? Puedes consultarlo en:",
+    language: "Idiomas",
+    close:"Cerrar",
+    downloadTime: "Su descarga tomará",
+    hours:"horas",
+    minutes:"minutos",
+    seconds:"segundos",
+    day:'dias'
 }
 
 const franchConfig = {
-    Peso: "Poids du fichier",
-    Velocidad: "Vitesse Internet",
-    Calcular: "Calculer",
-    Resultado: "Ici, vous verrez combien de temps votre téléchargement prendra.",
-    Verificar: "Vous ne savez pas quel est votre débit Internet? Vous pouvez le vérifier à l'adresse:",
-    Idiomas: "Langues"
+    weight:"Poids du fichier",
+    speed:"Vitesse Internet",
+    calculate:"Calculer",
+    results:"Ici, vous verrez combien de temps votre téléchargement prendra.",
+    doYouKnow:"Vous ne savez pas quel est votre débit Internet? Vous pouvez le vérifier à l'adresse:",
+    language:"Langues",
+    close:"Fermer",
+    downloadTime: "Votre téléchargement prendra",
+    hours:"Heures",
+    minutes:"minutes",
+    seconds:"secondes",
+    day:'jours'
+    
 }
 
 //switch languages
 
-function switchLanguages(lang){
-    languages = '';
-    
-    if(lang == 'ES'){
-        languages=spanishConfig;
-    } else if (lang == 'EN'){
-        languages=englishConfig;
-    }else if (lang=='FR'){
-        languages=franchConfig;
-    }  
-}
 
-//Selector of data-i18n attribute
+
+//Extraer idioma del navegador
+var idioma = navigator.language || navigator.userLanguage;
+idioma = idioma.substring(0,2);
+
 const elements = document.querySelectorAll("[data-i18n]");
 
-//Replace text function
-const replaceText = (el) => {
-    const key = el.innerText;
-    el.innerText = spanishConfig[key] || key;
+
+
+function switchLanguages(lang){
+    var language = lang;
+    const replaceText = (el) => {
+        const key = el.innerText;
+        switch (language) {
+            case 'es':
+                el.innerText = spanishConfig[key] || key;
+                break;
+            case 'en':
+                el.innerText = englishConfig[key] || key;
+                break;
+            case 'fr':
+                el.innerText = franchConfig[key] || key;
+                break;
+        }
+    
+    }
+    elements.forEach(el => replaceText(el));
 }
-elements.forEach(el => replaceText(el));
-
-
-
-
-
 
 
